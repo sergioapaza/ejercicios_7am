f9de8edbde7faf59e0332ba94b36fc601dabffa6
sergioapaza <47261716+sergioapaza@users.noreply.github.com>
2024-04-04 08:15:54 -0500

    Update anio.h

--- problema 2/anio.h
+++ problema 2/anio.h
@@ -4,41 +4,153 @@ using namespace std;
 
 class Anio {
     public:
-        explicit Anio(int d, int m,int y):day{d},month{m}{
+        explicit Anio(int d, int m,int y)
+        {
 
-
-            if(y>=1900 && y<=2050){
+            if(y>=1900 && y<=2050)
+            {
                 year=y;
+            }
+            if(m>=1 && m<=12)
+            {
+                month=m;
+                switch (m)
+                {
+                case 1:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+
+                case 2:
+                    if(isLeapYear()==1)
+                    {
+                        if(d>=1 && d<=29)
+                        {
+                            day=d;
+                        }
+                    }else{
+                        if(d>=1 && d<=28)
+                        {
+                            day=d;
+                        }
+                    }
+                    break;
+
+                case 3:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+
+                case 4:
+                    if(d>=1 && d<=30)
+                    {
+                        day=d;
+                    }
+                    break;
+                
+                case 5:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+                
+                case 6:
+                    if(d>=1 && d<=30)
+                    {
+                        day=d;
+                    }
+                    break;
+                case 7:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+                
+                case 8:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+                case 9:
+                    if(d>=1 && d<=30)
+                    {
+                        day=d;
+                    }
+                    break;
+                
+                case 10:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+                case 11:
+                    if(d>=1 && d<=30)
+                    {
+                        day=d;
+                    }
+                    break;
+                
+                case 12:
+                    if(d>=1 && d<=31)
+                    {
+                        day=d;
+                    }
+                    break;
+
+                default:
+                    break;
+                }
+            }
+            
+            
+            if(d>=1900 && d<=2050)
+            {
+                day=d;
             }
         }
 
 
-    void setDay(int d){
+    void setDay(int d)
+    {
         day=d;
     }
-    void setMonth(int m){
+    void setMonth(int m)
+    {
         month=m;
     }
-    void setYear(int y){
-        if(y>=1900 && y<=2050){
+    void setYear(int y)
+    {
+        if(y>=1900 && y<=2050)
+        {
                 year=y;
         }
     }
-    int getDay() const{
+    int getDay() const
+    {
         return day;
     }
-    int getMonth() const{
+    int getMonth() const
+    {
         return month;
     }
-    int getYear() const{
+    int getYear() const
+    {
         return year;
     }
 
 
-    bool isLeapYear(int y) const{
-        if(y%4==0){
-            if(y%100==0 ){
-                if(y%400==0 ){
+    bool isLeapYear() const{
+        if(year%4==0){
+            if(year%100==0 ){
+                if(year%400==0 ){
                     return true;
                 }
                 else{
@@ -64,8 +176,8 @@ class Anio {
 
    
     private:
-        int day;
-        int month;
+        int day{1};
+        int month{1};
         int year{1900};
        
 };
