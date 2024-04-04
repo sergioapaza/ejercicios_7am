a33636670147bb8cf46bde3e62017f2b4c0b5ae4
sergioapaza <47261716+sergioapaza@users.noreply.github.com>
2024-04-04 08:16:29 -0500

    Update hola2.cpp

--- problema 2/hola2.cpp
+++ problema 2/hola2.cpp
@@ -7,11 +7,11 @@ int main(){
     int mes=5;
     int anio=2048;
     Anio fechaUno{dia,mes,anio};
-    fechaUno.isLeapYear(anio);
+    fechaUno.isLeapYear();
     cout<<endl;
 
     cout<<"si es 1 es un anio bisiesto, si es 0 no es bisisesto."<<endl;
-    cout<<fechaUno.isLeapYear(anio);
+    cout<<fechaUno.isLeapYear();
     fechaUno.displayDate();
    
     return 0;
