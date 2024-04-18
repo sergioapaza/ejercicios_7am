b2d09cf72ec5b0706b71451ec8172e8432577b45
sergioapaza <47261716+sergioapaza@users.noreply.github.com>
2024-04-18 08:05:16 -0500

    Update nPrimo.cpp

--- Semana 6/nPrimo.cpp
+++ Semana 6/nPrimo.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 bool esPrimo(int n) {
@@ -38,8 +39,51 @@ void printNPrimos(int n) {
         }
         a++;
     }
-    
+}
 
+int contNum(string a){
+    
+    int counter{0};
+    for( int i = 0; i < a.length()  ; i++ ) {                
+        int val = static_cast<int>(a.at(i));
+        if(val >= 48 && val <= 59)
+            counter++;
+    }   
+    return counter;
+}
+
+string Mayu(string a){
+    char c;
+    for( int i = 0; i < a.size(); i++ ) {        
+        c = a.at(i);
+        int val = static_cast<int>(c);
+        if(val >= 97 && val <= 122)
+            a.at(i) = c - 32;  //toupper(c);
+    }
+    
+    return a;
+}
+
+int fib(int a){
+
+    if(a==0 || a==1){
+        return 1;
+    }else{
+        return fib(a-2)+fib(a-1);
+    }
+    /*int t1=1;
+    int t2=1;
+    int t3;
+
+    for(int i=1;i<=a;i++){
+        if( i == 1 || i==2){            
+            continue; 
+        }
+        t3 = t1 + t2;
+        t1 = t2;
+        t2 = t3;
+    }
+    cout << "El " << a << " fib es: " << t3;*/
 }
 
 int main(){
@@ -50,12 +94,22 @@ int main(){
     cout<<"Introduce el numero: ";
     cin>>cant;
 
-    printNPrimos(cant);
+    /*string pal;
+    cout << "Ingrese una cadena por favor: ";
+    getline(cin, pal);*/
+
+    //cout<<Mayu(pal);
+
+    //printNPrimos(cant);
+
     /*if( esPrimo(numero) ) {
         cout << numero << " ES PRIMO" << endl;
     }else{
         cout << numero << " NO ES PRIMO" << endl;
     }*/
+
+    cout<<fib(cant);
+
 
 
     return 0;
